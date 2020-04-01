49efbb9687c862c820876cd8d5105c7b12cd5b74
cloudcomputingID <alex@cloudcomputing.id>
2020-04-01 15:23:35 +0700

    Vehicle Model & Web Config

---
+++ Training/Models/Vehicle.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Training.Models
+{
+    public class Vehicle
+    {
+        [Key]
+        public string IdVehicle { get; set; }
+        public string Name { get; set; }
+        public string Descriptions { get; set; }
+        public int Wheel { get; set; }
+    }
+}
