dc57cb409ee8d2368150c667e4ca481b89e1881b
cloudcomputingID <alex@cloudcomputing.id>
2020-04-01 11:56:07 +0700

    Initiate Training

---
+++ Training/Controllers/PersonsController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Training.Models;
+
+namespace Training.Controllers
+{
+    public class PersonsController : Controller
+    {
+        // GET: Persons
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Show(string id, string name, string address)
+        {
+            var person = new Person()
+            {
+                IdPerson = id,
+                FullName = name,
+                Address = address
+            };
+            return View(person);
+        }
+    }
+}
